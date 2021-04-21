22261434b4d5fdc50147e6bb3aaf470d3245d61e
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2021-04-21 12:42:56 +0200

    Todo list by item

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import todo_list
+from .views import todo_list, todo_detail
 
 app_name = 'todos'
 
 urlpatterns = [
-    path('', todo_list)
+    path('', todo_list),
+    path('<id>/', todo_detail)
 ]
--- todo/views.py
+++ todo/views.py
@@ -10,3 +10,12 @@ def todo_list(request):
         "todo_list": todos
     }
     return render(request, "todo/todo_list.html", context)
+
+    #CRUD - Create, Retrieve, Update, Delete, ;;; List
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id = id)
+    context = {
+        "todo": todo
+    }
+    return render(request, "todo/todo_detail.html", context)
